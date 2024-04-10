a3423abe4712ce43c8e8eb8c6c83e6d0ca7a24c1
Shinwoo Kim <shinwookim@pitt.edu>
2024-04-10 18:25:23 -0400

    experiment results

--- speculative-decoding/speculative_decoding.py
+++ speculative-decoding/speculative_decoding.py
@@ -108,6 +108,97 @@ def speculative_decoding(
     seq_len = prefix.shape[1]
     T = seq_len + max_len
 
+    tokens_generated = 0
+    tokens_accepted = 0
+
+    assert prefix.shape[0] == 1, "input batch size must be 1"
+
+    # with tqdm(total=T, desc="speculative sampling") as pbar:
+    while prefix.shape[1] < T:
+        # q = M_q[prefix + x_0, x_1, .., x_(gamma-2)]
+        x = prefix
+        prefix_len = prefix.shape[1]
+        for _ in range(gamma):
+            # p.logits shape (batch, seq, vocab)
+            q = approx_model(x).logits
+            next_tok = sample(norm_logits(q[:, -1, :], temperature, top_k, top_p))
+            x = torch.cat((x, next_tok), dim=1)
+        tokens_generated = tokens_generated + gamma
+
+        # normalize the logits
+        for i in range(q.shape[1]):
+            q[:, i, :] = norm_logits(q[:, i, :], temperature, top_k, top_p)
+        # p  = M_p[prefix + x_0, x_0, .., x_(gamma-1)]
+        p = target_model(x).logits
+        for i in range(p.shape[1]):
+            p[:, i, :] = norm_logits(p[:, i, :], temperature, top_k, top_p)
+
+        # n the end position of the valid prefix
+        # x = x_[:prefix_len-1] + x_0, ... x_(gamma-1)
+
+        is_all_accept = True
+        n = prefix_len - 1
+        for i in range(gamma):
+            r = torch.rand(1, device=p.device)
+            j = x[:, prefix_len + i]
+
+            if r < torch.min(
+                torch.tensor([1], device=q.device),
+                p[:, prefix_len + i - 1, j] / q[:, prefix_len + i - 1, j],
+            ):
+                # accept, and update n
+                n += 1
+                tokens_accepted = tokens_accepted + 1
+            else:
+                # reject
+                t = sample(max_fn(p[:, n, :] - q[:, n, :]))
+                is_all_accept = False
+                break
+
+        prefix = x[:, : n + 1]
+
+        if is_all_accept:
+            t = sample(p[:, -1, :])
+
+        prefix = torch.cat((prefix, t), dim=1)
+        # pbar.update(n - pbar.n)
+    return (prefix, (tokens_accepted / tokens_generated))
+
+
+@torch.no_grad()
+def speculative_decoding_with_print(
+    prefix: torch.Tensor,
+    approx_model: torch.nn.Module,
+    target_model: torch.nn.Module,
+    max_len: int,
+    decoder,
+    gamma: int = 4,
+    temperature: float = 1,
+    top_k: int = 0,
+    top_p: float = 0,
+) -> torch.Tensor:
+    """
+    DeepMind version Speculative Sampling.
+    Accelerating Large Language Model Decoding with Speculative Sampling
+    https://arxiv.org/abs/2302.01318
+    No KV Cache Optimization
+
+    Args:
+        x (torch.Tensor): input sequence, (batch, prefix_seqlen), Note that the batch dim is always 1 now.
+        approx_model (torch.nn.Module): approx model, the small one
+        target_model (torch.nn.Module): target model, the large one
+        max_len (int): the max overall generated tokens number.
+        gamma (int): $\gamma$, the token number small model guesses.
+        temperature (float, optional): Defaults to 1.
+        top_k (int, optional): Defaults to 0.
+        top_p (float, optional): Defaults to 0.
+
+    Returns:
+        torch.Tensor: generated tokens (batch, target_seqlen)
+    """
+    seq_len = prefix.shape[1]
+    T = seq_len + max_len
+
     assert prefix.shape[0] == 1, "input batch size must be 1"
 
     # with tqdm(total=T, desc="speculative sampling") as pbar:
